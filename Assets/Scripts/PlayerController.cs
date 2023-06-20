10e484594e2dba81245ebc7bc2e11873c378b34f
Horacio Valdes <horavaldesg@gmail.com>
2023-06-20 02:09:45 -0400

    Added Temp UI for options menu and some scripts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OptionsManager : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     
 }
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance;
+    
+    [SerializeField] private GameObject optionsMenu;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void OptionsToggle(bool state)
+    {
+        //Toggles options menu depending on state recieved
+        optionsMenu.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        //Restarts Level
+    }
+
+    public void Quit()
+    {
+        //Returns to main menu
+    }
+}
