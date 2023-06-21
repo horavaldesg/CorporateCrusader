e2c15f61d6b561779d2eef1d3d537f58c9799414
Horacio Valdes <horavaldesg@gmail.com>
2023-06-21 19:30:42 -0400

    UI Options Scripts

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,27 +2,48 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
     public static UIManager Instance;
     
     [SerializeField] private GameObject optionsMenu;
+    private PlayerControls _controls;
 
     private void Awake()
     {
         Instance = this;
+        _controls = new PlayerControls();
+        _controls.Player.Escape.performed += tgb => OptionsToggle();
     }
-
+    
     public void Play()
     {
         //Play
     }
 
-    public void OptionsToggle(bool state)
+    private void OptionsToggle()
     {
         //Toggles options menu depending on state recieved
-        optionsMenu.SetActive(true);
+        optionsMenu.SetActive(!optionsMenu.activeSelf);
+    }
+    
+    private void OnEnable()
+    {
+        //Enables Player Input
+        _controls.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        //Disables Player Input
+        _controls.Player.Disable();
+    }
+
+    public void Resume()
+    {
+        OptionsToggle();
     }
 
     public void Restart()
@@ -30,8 +51,9 @@ public class UIManager : MonoBehaviour
         //Restarts Level
     }
 
-    public void Quit()
+    public void QuitToMainMenu()
     {
         //Returns to main menu
+        SceneManager.LoadScene("MainMenu");
     }
 }
