45789904aba15c801f411153ef48598280dbb74a
Horacio Valdes <horavaldesg@gmail.com>
2023-06-21 20:44:58 -0400

    Temp Main menu ui

---
+++ Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private GameObject landscapeUI;
+    [SerializeField] private GameObject portraitUI;
+    
+   private ScreenOrientation CurrentScreenOrientation
+   {
+      get{
+#if UNITY_EDITOR
+                if(Screen.height > Screen.width){
+                    return ScreenOrientation.Portrait;
+                } else {
+                    return ScreenOrientation.LandscapeLeft;
+                }
+#else
+         return Screen.orientation;
+#endif
+      }
+   }
+
+   private void Update()
+   {
+       if (CurrentScreenOrientation == ScreenOrientation.Portrait)
+       {
+           portraitUI.SetActive(true);
+           landscapeUI.SetActive(false);
+       }
+       else
+       {
+           portraitUI.SetActive(false);
+           landscapeUI.SetActive(true);
+       }
+   }
+}
