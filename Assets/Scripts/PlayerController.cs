e9086886139314cc7e566589d15cb9b4c6868e96
Horacio Valdes <horavaldesg@gmail.com>
2023-06-21 21:39:45 -0400

    Temp Sprites with character flip and gun rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,16 +6,22 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float playerSpeed;
+    [SerializeField] private Transform gunRotate;
     [SerializeField][Range(0,1)] private float smoothMovement;
+    [SerializeField] private float gunRotationSpeed;
     private PlayerControls _controls;
     private Vector2 _move;
     private CharacterController _cc;
     private Vector3 _dampSpeed;
     private Vector3 _currentVelocity;
+    private SpriteRenderer playerSprite;
+    private SpriteRenderer gunRenderer;
     
     private void Awake()
     {
         TryGetComponent(out _cc);
+        TryGetComponent(out playerSprite);
+        gunRotate.TryGetComponent(out gunRenderer);
         _controls = new PlayerControls();
         //Movement Player Input
         _controls.Player.Move.performed += tgb => { _move = tgb.ReadValue<Vector2>(); };
@@ -44,7 +50,7 @@ public class PlayerController : MonoBehaviour
     {
         //Worlds Space of player
         var movement = transform.TransformDirection(_move);
-        
+
         //Speed damp
         _currentVelocity = Vector3.SmoothDamp
         (
@@ -53,8 +59,19 @@ public class PlayerController : MonoBehaviour
             ref _dampSpeed,
             smoothMovement
         );
-        
+
         //Moves Player
         _cc.Move(_currentVelocity * Time.deltaTime);
+        if (_cc.velocity.magnitude == 0) return;
+        playerSprite.flipX = _move.x < 0;
+        RotateGun();
+    }
+
+    private void RotateGun()
+    {
+        var moveDir = new Vector2(-_move.y, _move.x);
+        gunRenderer.flipY = moveDir.y < 0;
+        Quaternion rotation = Quaternion.LookRotation(Vector3.forward, moveDir);
+        gunRotate.rotation = Quaternion.RotateTowards(gunRotate.transform.rotation, rotation, gunRotationSpeed * Time.deltaTime * 100);
     }
 }
