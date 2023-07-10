5dcdd3d03d12a72fadae9b7b9f9fff74b9142819
Horacio Valdes <horavaldesg@gmail.com>
2023-07-09 22:43:45 -0400

    Nail Gun somewhat working

--- Assets/Scripts/Weapons/Construction Weapons/NailGun.cs
+++ Assets/Scripts/Weapons/Construction Weapons/NailGun.cs
@@ -9,33 +9,44 @@ public class NailGun : SelectedWeapon
 {
     public int distanceFromPlayer;
     private int _baseDistance;
-    
+
     private float _angle;
     private float _baseAngle;
-    
+
     protected override void Start()
     {
-        _angle = PlayerController.Instance.GunRotation().normalized.y - 0.2f;
+        _angle = 0.8f;
         _baseAngle = _angle;
         _baseDistance = distanceFromPlayer;
+
         base.Start();
     }
-
-    private void FixedUpdate()
+    private void InstantiateObjectsOnHalfCircle()
     {
-        // transform.rotation = PlayerController.Instance.CurrentGunRotation.rotation;
+        var numberOfObjects = 10; // Number of objects to instantiate
+        var centerPoint = transform.position + (transform.right + transform.up).normalized * 0.5f;
+        var angleIncrement = 180f / numberOfObjects;
+        for (var i = 0; i < numberOfObjects; i++)
+        {
+            var angle = transform.eulerAngles.z + (i * angleIncrement);
+            var radianAngle = Mathf.Deg2Rad * angle;
+            var x = centerPoint.x + Mathf.Sin(radianAngle) * 3;
+            var y = centerPoint.y + Mathf.Cos(radianAngle) * -3;
+            var z = centerPoint.z;
+            var position = new Vector3(x, y, z);
+            var go = Instantiate(instantiatedObject);
+            go.transform.position = position;
+            go.transform.rotation = transform.rotation;
+            Destroy(go, 2);
+        }
     }
-
+    
     protected override void Activate()
     {
-        _angle = PlayerController.Instance.GunRotation().normalized.y - 0.2f;
-        _baseAngle = _angle;
-        for (var i = 0; i < 6; i++)
-        {
-            InstantiateObjects();
-            _angle -= 0.3f;
-        }
-        
+        InstantiateObjectsOnHalfCircle();
+           // _angle -= 0.3f;
+       
+
         base.Activate();
     }
 
@@ -60,13 +71,13 @@ public class NailGun : SelectedWeapon
         go.TryGetComponent(out NailGunProjectile nailGunProjectile);
         nailGunProjectile.playerDirection = PlayerController.Instance.DirectionOfPlayer();
 
-        var randomPoint = RandomPointOnXZCircle(transform.position, distanceFromPlayer);
+        var randomPoint = RandomPointOnXZCircle(PlayerController.Instance.CurrentPlayerTransform().position, distanceFromPlayer);
         go.transform.position = randomPoint;
         go.transform.rotation = quaternion.Euler(0,0,_angle);
         Destroy(go, 3);
     }
 
-    private Vector3 RandomPointOnXZCircle(Vector3 center, float radius) {
+    Vector3 RandomPointOnXZCircle(Vector3 center, float radius) {
         return center + new Vector3(Mathf.Cos(_angle), Mathf.Sin(_angle),1 ) * radius;
     }
 }
