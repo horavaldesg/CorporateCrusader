678f0186c1ab771345ce17d043e538bd4d8eed09
Horacio Valdes <horavaldesg@gmail.com>
2023-07-26 18:14:58 -0400

    Anchor

--- Assets/Scripts/Weapons/Sailor Weapons/Anchor.cs
+++ Assets/Scripts/Weapons/Sailor Weapons/Anchor.cs
@@ -2,17 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Anchor : MonoBehaviour
+public class Anchor : SelectedWeapon
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float throwForce;
+    
+    protected override void Activate()
     {
-        
+        Throw();
+        base.Activate();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Throw()
     {
-        
+        transform.eulerAngles = GetRandomRotation();
+        var go = Instantiate(instantiatedObject);
+        go.transform.position = PlayerController.Instance.CurrentPlayerTransform().position;
+        go.transform.eulerAngles = transform.eulerAngles;
+        go.TryGetComponent(out Rigidbody2D rb);
+        go.TryGetComponent(out AnchorThrowable anchorThrowable);
+        anchorThrowable.damage = damage;
+        rb.AddForce(go.transform.right * (throwForce * 100));
+    }
+
+    private void FixedUpdate()
+    {
+        var playerPos = PlayerController.Instance.CurrentPlayerTransform().position;
+        transform.position = playerPos;
+    }
+
+    private Vector3 GetRandomRotation()
+    {
+        return new Vector3(0, 0, Random.Range(0, 180));
     }
 }
---
+++ Assets/Scripts/Weapons/Sailor Weapons/AnchorThrowable.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(
+    typeof(Rigidbody2D),
+    typeof(CircleCollider2D))]
+public class AnchorThrowable : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    public float damage;
+
+    private void Start()
+    {
+        TryGetComponent(out CircleCollider2D circleCollider2D);
+        circleCollider2D.isTrigger = true;
+        Destroy(gameObject, 5);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(!other.CompareTag("Enemy"))return;
+        other.TryGetComponent(out Enemy enemy);
+        enemy.TakeDamage(damage);
+        Destroy(gameObject);
+    }
+}
