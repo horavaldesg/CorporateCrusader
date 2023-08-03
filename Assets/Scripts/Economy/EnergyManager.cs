5433070ca2d89921aa5fc184eaddd90fa988a0d9
Matt-Camerato <matt.camerato@gmail.com>
2023-08-03 14:47:12 -0400

    Energy System Finishing Touches
    Finished up the energy system by adding limits so that you can never have negative energy or more than your max limit. I also added a timer that displays below your energy on the top bar whenever you have less than 30 energy and it shows the time until you will recharge another 5 energy, which happens every 5 minutes.

--- Assets/Scripts/Economy/EnergyManager.cs
+++ Assets/Scripts/Economy/EnergyManager.cs
@@ -3,12 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Services.Authentication;
+using TMPro;
 
 public class EnergyManager : MonoBehaviour
 {
     [SerializeField] private int maxEnergy = 30;
     [SerializeField] private int energyEarnedPerTimespan = 5;
     [SerializeField] private float secondsToEarnEnergy = 300;
+    [SerializeField] private TMP_Text rechargeTimerText;
 
     private DateTime currentTime;
 
@@ -18,6 +20,14 @@ public class EnergyManager : MonoBehaviour
     private void Start()
     { 
         AuthenticationService.Instance.SignedIn += LoadEnergy;
+    }
+
+    public void ChangeEnergyDevButton(int amount)
+    {
+        int energyToChange = amount;
+        if(energyToChange > maxEnergy - ProfileManager.Instance.ProfileInfo.energy) energyToChange = maxEnergy - ProfileManager.Instance.ProfileInfo.energy;
+        if(ProfileManager.Instance.ProfileInfo.energy + energyToChange < 0) energyToChange = 0;
+        ProfileManager.Instance.ChangeNumEnergy(energyToChange);
     }
 
     private void LoadEnergy()
@@ -37,7 +47,7 @@ public class EnergyManager : MonoBehaviour
             TimeSpan difference = currentTime.Subtract(oldTime);
 
             //use number of seconds passed to make energy recharge while offline
-            float seconds = (float) difference.TotalSeconds + (300 - PlayerPrefs.GetFloat("OldRechargeTime"));
+            float seconds = (float) difference.TotalSeconds + (secondsToEarnEnergy - PlayerPrefs.GetFloat("OldRechargeTime"));
             int energyEarned = energyEarnedPerTimespan * (Mathf.FloorToInt(seconds / secondsToEarnEnergy));
             int energyToAdd = Mathf.Clamp(energyEarned, 0, maxEnergy - ProfileManager.Instance.ProfileInfo.energy);
             ProfileManager.Instance.ChangeNumEnergy(energyToAdd);
@@ -49,20 +59,47 @@ public class EnergyManager : MonoBehaviour
         }
     }
 
+    private IEnumerator AttemptRecharge()
+    {
+        while(ProfileManager.Instance.ProfileInfo.energy == maxEnergy)
+        {
+            rechargeTimerText.enabled = false; //disable recahrge timer text if player has max energy
+            yield return null;;
+        }
+
+        yield return RechargeEnergy(secondsToEarnEnergy);
+    }
+
     private IEnumerator RechargeEnergy(float time)
     {
-        //wait for time to expire
+        //set initial recharge time
         rechargeTime = time;
+        int minutes = Mathf.FloorToInt(rechargeTime / 60);
+        int seconds = Mathf.FloorToInt(rechargeTime % 60);
+        rechargeTimerText.text = string.Format("{0}:{1:00}", minutes, seconds); //rechargeTS.Minutes + ":" + rechargeTS.Seconds;
+
+        //loop during recharge time
         while(rechargeTime > 0)
         {
+            //decrement recharge time
             rechargeTime -= Time.deltaTime;
+
+            //check if player still needs to recharge energy
+            if(ProfileManager.Instance.ProfileInfo.energy == maxEnergy) yield return AttemptRecharge(); //attempt another recharge
+
+            //update recharge timer text
+            rechargeTimerText.enabled = true;
+            minutes = Mathf.FloorToInt(rechargeTime / 60);
+            seconds = Mathf.FloorToInt(rechargeTime % 60);
+            rechargeTimerText.text = string.Format("{0}:{1:00}", minutes, seconds);
+
             yield return null;
         }
         
         //determine amount of energy to restore, making sure not to go over 30
         int energyToAdd = Mathf.Clamp(energyEarnedPerTimespan, 0, maxEnergy - ProfileManager.Instance.ProfileInfo.energy);
         ProfileManager.Instance.ChangeNumEnergy(energyToAdd); //restore energy
-        yield return RechargeEnergy(secondsToEarnEnergy); //repeat recharge on 5 minute timer
+        yield return AttemptRecharge(); //attempt another recharge
     }
 
     private void OnDestroy()
