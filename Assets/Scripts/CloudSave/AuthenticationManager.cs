3a39affcbb065b601dae22d9c979c9648d47d692
Horacio Valdes <horavaldesg@gmail.com>
2023-08-07 18:27:23 -0400

    Apple Auth Player Name

--- Assets/Scripts/CloudSave/AuthenticationManager.cs
+++ Assets/Scripts/CloudSave/AuthenticationManager.cs
@@ -106,6 +106,7 @@ public class AuthenticationManager : MonoBehaviour
                         appleIDCredential.IdentityToken.Length);
                     Debug.Log("Sign-in with Apple successfully done. IDToken: " + idToken);
                     Token = idToken;
+                    SignInWithApple(Token);
                     SetPlayerName(appleIDCredential);
                     mainMenuManager.LoginScreenToStageSelect();
                 }
@@ -123,7 +124,38 @@ public class AuthenticationManager : MonoBehaviour
         );
     }
 
+    private async void SignInWithApple(string idToken)
+    {
+        await SignInWithAppleAsync(idToken);
+    }
+    
     private async void SetPlayerName(IAppleIDCredential appleIDCredential)
+    {
+        await SetPlayerNameAsync(appleIDCredential);
+    }
+    
+    private async Task SignInWithAppleAsync(string idToken)
+    {
+        try
+        {
+            await AuthenticationService.Instance.SignInWithAppleAsync(idToken);
+            Debug.Log("SignIn is successful.");
+        }
+        catch (AuthenticationException ex)
+        {
+            // Compare error code to AuthenticationErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+        catch (RequestFailedException ex)
+        {
+            // Compare error code to CommonErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+    }
+    
+    private async Task SetPlayerNameAsync(IAppleIDCredential appleIDCredential)
     {
         await AuthenticationService.Instance.UpdatePlayerNameAsync(appleIDCredential.FullName.GivenName);
     }
