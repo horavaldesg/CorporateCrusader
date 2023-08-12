3015a043222dbe8970585b15cb4cac3932a33d58
Horacio Valdes <horavaldesg@gmail.com>
2023-08-11 21:19:56 -0400

    Orientation Manager:

--- Assets/Scripts/UI/OrientationManager.cs
+++ Assets/Scripts/UI/OrientationManager.cs
@@ -8,17 +8,22 @@ public class OrientationManager : MonoBehaviour
  
     // This event will only be called when an orientation changed (i.e. won't be call at launch)
     public static event UnityAction<ScreenOrientation> orientationChangedEvent;
- 
+    public static event Action<ScreenOrientation> OnOrientationChange;
+
     [SerializeField] private bool _debugMode = false;
  
     public ScreenOrientation orientation;
  
     private void Start()
     {
+        ToggleRefresh(true);
+        Screen.autorotateToPortraitUpsideDown = false;
         orientation = Screen.orientation;
         InvokeRepeating("CheckForChange", 1, 1);
     }
- 
+
+   
+
     private static void OnOrientationChanged(ScreenOrientation orientation)
     {
         if (orientationChangedEvent != null)
@@ -29,14 +34,24 @@ public class OrientationManager : MonoBehaviour
     {
         if (_debugMode)
             Debug.Log("Screen.orientation=" + Screen.orientation);
+        ToggleRefresh(true);
         if (orientation != Screen.orientation) {
             orientation = Screen.orientation;
-            OnOrientationChanged(orientation);
+            OnOrientationChange?.Invoke(orientation);
+            ToggleRefresh(false);
+            //OnOrientationChanged(orientation);
         }
     }
- 
-    #if UNITY_EDITOR
- 
+
+    void ToggleRefresh(bool state)
+    {
+        Screen.autorotateToLandscapeLeft = state;
+        Screen.autorotateToLandscapeRight = state;
+        Screen.autorotateToPortrait = state;
+    }
+
+#if UNITY_EDITOR
+
     [ContextMenu("Print Orientation")]
     private void PrintOrientation()
     {
--- Assets/Scripts/UI/SafeArea.cs
+++ Assets/Scripts/UI/SafeArea.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace Crystal
 {
@@ -96,7 +98,7 @@ namespace Crystal
         };
         #endregion
 
-        RectTransform Panel;
+        [SerializeField]RectTransform[] Panel;
         Rect LastSafeArea = new Rect (0, 0, 0, 0);
         Vector2Int LastScreenSize = new Vector2Int (0, 0);
         ScreenOrientation LastOrientation = ScreenOrientation.AutoRotation;
@@ -106,7 +108,8 @@ namespace Crystal
 
         void Awake ()
         {
-            Panel = GetComponent<RectTransform> ();
+            //Panel = GetComponent<RectTransform> ();
+           
 
             if (Panel == null)
             {
@@ -114,16 +117,30 @@ namespace Crystal
                 Destroy (gameObject);
             }
 
-            Refresh ();
+           // Refresh();
         }
 
-        void Update ()
+        private void OnEnable()
         {
-            Refresh ();
+            OrientationManager.OnOrientationChange += Refresh;
         }
 
-        void Refresh ()
+        private void OnDisable()
         {
+            OrientationManager.OnOrientationChange -= Refresh;
+
+        }
+
+        IEnumerator RotationChanged()
+        {
+            yield return new WaitForSeconds(0.15f);
+            ToggleRefresh(true);
+        }
+
+
+        void Refresh (ScreenOrientation orientation)
+        {
+            //ToggleRefresh(false);
             Rect safeArea = GetSafeArea ();
 
             if (safeArea != LastSafeArea
@@ -135,10 +152,17 @@ namespace Crystal
                 // See https://forum.unity.com/threads/569236/#post-4473253 and https://forum.unity.com/threads/569236/page-2#post-5166467
                 LastScreenSize.x = Screen.width;
                 LastScreenSize.y = Screen.height;
-                LastOrientation = Screen.orientation;
-
-                ApplySafeArea (safeArea);
+                LastOrientation = orientation;
+                Debug.Log("New Screen Orientation " + Screen.orientation);
+                ApplySafeArea (Screen.safeArea);
             }
+        }
+
+        void ToggleRefresh(bool state)
+        {
+            Screen.autorotateToLandscapeLeft = state;
+            Screen.autorotateToLandscapeRight = state;
+            Screen.autorotateToPortrait = state;
         }
 
         Rect GetSafeArea ()
@@ -206,20 +230,23 @@ namespace Crystal
             // Check for invalid screen startup state on some Samsung devices (see below)
             if (Screen.width > 0 && Screen.height > 0)
             {
-                // Convert safe area rectangle from absolute pixels to normalised anchor coordinates
-                Vector2 anchorMin = r.position;
-                Vector2 anchorMax = r.position + r.size;
-                anchorMin.x /= Screen.width;
-                anchorMin.y /= Screen.height;
-                anchorMax.x /= Screen.width;
-                anchorMax.y /= Screen.height;
+                foreach (var panels in Panel) {
+                    if (!panels) return;
+                    // Convert safe area rectangle from absolute pixels to normalised anchor coordinates
+                    Vector2 anchorMin = r.position;
+                    Vector2 anchorMax = r.position + r.size;
+                    anchorMin.x /= Screen.width;
+                    anchorMin.y /= Screen.height;
+                    anchorMax.x /= Screen.width;
+                    anchorMax.y /= Screen.height;
 
-                // Fix for some Samsung devices (e.g. Note 10+, A71, S20) where Refresh gets called twice and the first time returns NaN anchor coordinates
-                // See https://forum.unity.com/threads/569236/page-2#post-6199352
-                if (anchorMin.x >= 0 && anchorMin.y >= 0 && anchorMax.x >= 0 && anchorMax.y >= 0)
-                {
-                    Panel.anchorMin = anchorMin;
-                    Panel.anchorMax = anchorMax;
+                    // Fix for some Samsung devices (e.g. Note 10+, A71, S20) where Refresh gets called twice and the first time returns NaN anchor coordinates
+                    // See https://forum.unity.com/threads/569236/page-2#post-6199352
+                    if (anchorMin.x >= 0 && anchorMin.y >= 0 && anchorMax.x >= 0 && anchorMax.y >= 0)
+                    {
+                        panels.anchorMin = anchorMin;
+                        panels.anchorMax = anchorMax;
+                    }
                 }
             }
 
@@ -228,6 +255,7 @@ namespace Crystal
                 Debug.LogFormat ("New safe area applied to {0}: x={1}, y={2}, w={3}, h={4} on full extents w={5}, h={6}",
                 name, r.x, r.y, r.width, r.height, Screen.width, Screen.height);
             }
+            //StartCoroutine(RotationChanged());
         }
     }
 }
