1484e9a8b3158cd5eeb924accd5d27ffe3468444
Horacio Valdes <horavaldesg@gmail.com>
2023-08-25 19:03:56 -0400

    Removed require box collider for bullet

--- Assets/Scripts/Weapons/Temp Weapons/Bullet.cs
+++ Assets/Scripts/Weapons/Temp Weapons/Bullet.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class Bullet : MonoBehaviour
 {
     public float Damage { get; set; }
